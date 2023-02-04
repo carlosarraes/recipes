19f42a090790266e192d01bbbbadc109862987d1
carraes <carraeshb@gmail.com>
2023-02-04 09:59:40 -0300

    chore: initial files

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+  },
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Aplicativo de Receitas
+
+Bem-vindo ao aplicativo de receitas! Este projeto foi desenvolvido para ajudá-lo a encontrar e salvar suas receitas favoritas.
+
+## Tecnologias Utilizadas
+
+- React Typescript
+- React Router
+- React Context
+- Tailwind CSS
+- DaisyUI
+
+## Características
+
+- Consulta duas bases de dados: "themealdb" e "thecocktaildb".
+- Possibilidade de fazer login (mock).
+- Salvar receitas.
+- Acompanhar as receitas já feitas.
+- Compartilhar receitas com amigos.
+- Adicionar receitas à lista de favoritos.
+
+## Cobertura de Testes
+
+- Desenvolvendo
