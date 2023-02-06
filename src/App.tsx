5b1b2b2b80cb9cb369c2d6dcb11d3b25242974f0
carraes <carraeshb@gmail.com>
2023-02-06 12:57:21 -0300

    refactor: new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Done from './pages/Done'
 import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import InProgress from './pages/InProgress'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import RecipeDetails from './pages/RecipeDetails'
@@ -17,6 +18,8 @@ function App() {
       <Route path="/perfil" element={<Profile />} />
       <Route path="/favoritos" element={<Favorites />} />
       <Route path="/preparados" element={<Done />} />
+      <Route path="/meals/:id/in-progress" element={<InProgress />} />
+      <Route path="/drinks/:id/in-progress" element={<InProgress />} />
     </Routes>
   )
 }
