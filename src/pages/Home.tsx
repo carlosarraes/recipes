77a9ec6a300f44e46ece25d7cc215feadbae98e4
carraes <carraeshb@gmail.com>
2023-02-06 01:13:08 -0300

    feat: pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { ToastContainer } from 'react-toastify'
+import CategoryButtons from '../components/CategoryButtons'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Recipes from '../components/Recipes'
+import 'react-toastify/dist/ReactToastify.css'
+
+interface HomeProps {
+  type: string
+}
+
+const Home = ({ type }: HomeProps) => {
+  return (
+    <main>
+      <Header type={type} />
+      <ToastContainer />
+      <CategoryButtons type={type} />
+      <Recipes type={type} />
+      <Footer />
+    </main>
+  )
+}
+
+export default Home
