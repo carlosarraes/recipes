52abeb18ae3ca061b83493e6273174849b6e5ad4
carraes <carraeshb@gmail.com>
2023-02-07 00:22:52 -0300

    refactor: RecipeDetails

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { fetchById } from '../utils/fetchService'
 import Carousel from './Carousel'
+import { AiOutlineHeart, AiFillHeart, AiFillCopy } from 'react-icons/ai'
+import copy from 'clipboard-copy'
+import { toast, ToastContainer } from 'react-toastify'
+import { BiArrowBack } from 'react-icons/bi'
 
 export interface Recipe {
   idMeal?: string
@@ -18,11 +22,14 @@ export interface Recipe {
   ingredients?: string[]
   measure?: string[]
   tags?: string[]
+  type?: string
+  doneDate?: string
 }
 
 const RecipeDetails = () => {
   const [showRecipe, setShowRecipe] = useState<Recipe>()
   const [started, setStarted] = useState(false)
+  const [favorite, setFavorite] = useState(false)
   const location = useLocation()
 
   const [, type, id] = location.pathname.split('/')
@@ -55,6 +62,7 @@ const RecipeDetails = () => {
         ingredients,
         measure,
         tags,
+        type,
       }
       setShowRecipe(newRecipe)
     }
@@ -71,6 +79,15 @@ const RecipeDetails = () => {
     })
     if (found != null) setStarted(true)
   }, [started])
+
+  useEffect(() => {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes') ?? '[]')
+    const found = favoriteRecipes.find((recipe: Recipe) => {
+      const idLookup = recipe.idMeal ?? recipe.idDrink
+      return idLookup === id
+    })
+    if (found != null) setFavorite(true)
+  }, [favorite])
 
   const handleStart = () => {
     const { idMeal, idDrink } = showRecipe ?? {}
@@ -91,8 +108,41 @@ const RecipeDetails = () => {
     navigate(`/${type}/${id as string}/in-progress`)
   }
 
+  const handleFavorite = () => {
+    if (localStorage.getItem('favoriteRecipes') === null) {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([showRecipe]))
+    } else {
+      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes') ?? '[]')
+      const newFavoriteRecipes = [...favoriteRecipes, showRecipe]
+      localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes))
+    }
+    setFavorite(true)
+  }
+
+  const handleUnfavorite = () => {
+    const { idMeal, idDrink } = showRecipe ?? {}
+    const id = idMeal ?? idDrink
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes') ?? '[]')
+    const newFavoriteRecipes = favoriteRecipes.filter((recipe: Recipe) => {
+      const idLookup = recipe.idMeal ?? recipe.idDrink
+      return idLookup !== id
+    })
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes))
+    setFavorite(false)
+  }
+
+  const handleCopy = () => {
+    copy(`http://localhost:3000/${type}/${id}`).catch((err) => console.log(err))
+    toast.success('Link copiado com sucesso!')
+  }
+
+  const handleGoBack = () => {
+    navigate('/')
+  }
+
   return (
     <section className="flex flex-col">
+      <ToastContainer />
       <div className="relative">
         <img
           src={showRecipe?.strMealThumb ?? showRecipe?.strDrinkThumb}
@@ -106,6 +156,29 @@ const RecipeDetails = () => {
         <p className="text-sm text-slate-800 absolute bottom-0 pl-2">
           {showRecipe?.strCategory} - {showRecipe?.strArea ?? showRecipe?.strAlcoholic}
         </p>
+        <div className="absolute top-0 right-0 flex">
+          <AiFillCopy
+            className="text-red-500 text-4xl m-1 cursor-pointer drop-shadow-lg"
+            onClick={handleCopy}
+          />
+          {favorite ? (
+            <AiFillHeart
+              className="text-red-500 text-4xl m-1 cursor-pointer drop-shadow-lg"
+              onClick={handleUnfavorite}
+            />
+          ) : (
+            <AiOutlineHeart
+              className="text-red-500 text-4xl m-1 cursor-pointer drop-shadow-lg"
+              onClick={handleFavorite}
+            />
+          )}
+        </div>
+        <div className="absolute top-0 left-0">
+          <BiArrowBack
+            className="text-red-500 text-4xl m-1 cursor-pointer drop-shadow-lg"
+            onClick={handleGoBack}
+          />
+        </div>
       </div>
       <div className="flex flex-col p-2 border border-slate-700 rounded-md m-2">
         <h2 className="text-xl font-bold text-slate-800">Ingredients</h2>
@@ -132,7 +205,7 @@ const RecipeDetails = () => {
           {showRecipe?.strInstructions}
         </p>
       </div>
-      {type === 'meals' && (
+      {(showRecipe?.tags?.length as number) >= 1 && (
         <div className="flex flex-col p-2">
           <h2 className="text-xl font-bold text-slate-800">Tags</h2>
           <p className="text-slate-800 text-sm border border-slate-700 rounded-md p-2 mt-1">
